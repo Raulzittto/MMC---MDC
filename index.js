131598c0ea50f182d74d4aadd7705a429dddffa0
Raul <soaresraul22@gmail.com>
2024-06-19 10:11:40 -0300

    add: adicionado o MDC

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
+console.log("Vamos calcular MDC ( mínimo múltiplo comum ) de dois numeros_");
+console.log("digite o primeiros numeros:");
+let numeros1 = "";
+let numeros2 = "";
+let entrada_usuário;
+let MDC;
+let MMC;
 
-console.log(
-    "Insira dois números para calcular seu MDC ( mínimo múltiplo comum )_"
-  );
-  let numeros = [];
-  let entrada_usuário;
-  let MDC;
-  
-  process.stdin.on("data", function (data) {
-    entrada_usuário = Number(data.toString().trim());
-    numeros.push(entrada_usuário);
-   for (let i = 0; i< numeros.length; i ++){
-for (let j =i + 1; j<numeros.length; j++ ){
-    if (numeros[i]< numeros[j]){
-        
+process.stdin.on("data", function (data) {
+  entrada_usuário = Number(data.toString().trim());
+  if (!numeros1) {
+    numeros1 = entrada_usuário;
+    console.log("digite o segundo número: ");
+  } else if (!numeros2) {
+    numeros2 = entrada_usuário;
+
+    while (numeros2 != 0) {
+      var j = numeros2;
+      numeros2 = numeros2 % numeros1;
+      numeros1 = j;
+      MDC = numeros1;
     }
-}
-   
-   }
-  });
+    console.log("O MDC será: " + MDC);
+  }
+
+});
